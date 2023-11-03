ec145eee26c0471d4b1bf6dcacec63d80bc34df5
orizvi4 <orizvi4@gmail.com>
2023-11-03 16:16:07 +0200

    removed unused

--- src/components/kibana/kibana.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@nestjs/common";
-import { KibanaService } from "./kibana.service";
-
-@Controller()
-export class KibanaController {
-    constructor(private kibanaService: KibanaService) {}
-}
